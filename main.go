682dd3532440fac263a20d2eb8a0707eb14ec5fe
Divyesh Patel <89864313+pateldivyesh12@users.noreply.github.com>
2022-04-25 13:33:33 -0500

    updated the source code

--- main.go
+++ main.go
@@ -186,7 +186,7 @@ func main() {
 	
 	//Option 4
 	//Database application running on Google Cloud Platform. 
-	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/chicago-business-intelligence:us-central1:mypostgres sslmode=disable port = 5432"
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/smart-window-348005:us-central1:mypostgres sslmode=disable port = 5432"
 	
 
 	db, err := sql.Open("postgres", db_connection)
